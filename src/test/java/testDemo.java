730761ecc684d02b2cfc5d3c1305bf43a2251da1
jagadeep <jagadeep2004@gmail.com>
2026-04-01 11:00:24 +0530

    Sample flipkart web test Demo

---
+++ src/test/java/testDemo.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.Keys;
+public class testDemo {
+	public static void main(String[] args) {
+		WebDriver driver = new  EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+//		WebElement search = driver.findElement(By.name("q"));
+//		search.sendKeys("Samsung",Keys.ENTER);
+		
+		String title = driver.getTitle();
+		System.out.println("page title is :"+ title +"Title length :"+title.length());
+		
+		//System.out.println("page current url is :"+driver.getCurrentUrl());
+		
+//		String page = driver.getPageSource();
+//		System.out.println("page page source is :"+ page +"Page length :"+ page.length());
+		
+		driver.close();
+	}
+
+}

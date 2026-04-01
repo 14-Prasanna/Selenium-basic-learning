8aa66b888b3b36dbf75c124b5ad1043bfaca5f5a
jagadeep <jagadeep2004@gmail.com>
2026-04-01 12:10:50 +0530

    new changes

--- src/test/java/testDemo.java
+++ src/test/java/testDemo.java
@@ -1,26 +1,26 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.Keys;
-public class testDemo {
-	public static void main(String[] args) {
-		WebDriver driver = new  EdgeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
-//		WebElement search = driver.findElement(By.name("q"));
-//		search.sendKeys("Samsung",Keys.ENTER);
-		
-		String title = driver.getTitle();
-		System.out.println("page title is :"+ title +"Title length :"+title.length());
-		
-		//System.out.println("page current url is :"+driver.getCurrentUrl());
-		
-//		String page = driver.getPageSource();
-//		System.out.println("page page source is :"+ page +"Page length :"+ page.length());
-		
-		driver.close();
-	}
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.junit.jupiter.api.Test;
+
+public class testDemo {
+
+    @Test
+    void testFlipkartTitle() {
+
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--headless=new"); // important for Jenkins
+        options.addArguments("--disable-gpu");
+
+        WebDriver driver = new EdgeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://www.flipkart.com/");
+
+        String title = driver.getTitle();
+        System.out.println("Page title: " + title + " | Length: " + title.length());
+
+        driver.quit();
+    }
 }
